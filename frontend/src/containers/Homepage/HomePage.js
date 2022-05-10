f5204db3cf3df3b2ee43051a20edad779255b722
Cong Thanh <pdcthanh112.dev@gmail.com>
2022-05-10 11:32:17 +0700

    09/05/2022

--- frontend/src/containers/Homepage/HomePage.js
+++ frontend/src/containers/Homepage/HomePage.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './HomePage.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import HomeHeader from './HomeHeader/HomeHeader';
+import Specialty from './Section/Specialty/Specialty';
+import MedicalFacility from './Section/MedicalFacility/MedicalFacility';
+import OutStandingDoctor from './Section/OutStandingDoctor/OutStandingDoctor';
+import Handbook from './Section/Handbook/Handbook';
 
 class HomePage extends Component {
 
     render() {
-
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 3,
+          };
         return (
-            <div>home page</div>
+            <div>
+                <HomeHeader/>
+                <Specialty settings={settings}/>
+                <MedicalFacility settings={settings}/>
+                <OutStandingDoctor settings={settings}/>
+                <Handbook settings={settings}/>
+            </div>
         );
     }
 
---
+++ frontend/src/containers/Homepage/Section/OutStandingDoctor/OutStandingDoctor.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./OutStandingDoctor.scss";
+import Slider from "react-slick";
+
+class OutStandingDoctor extends Component {
+  render() {
+    return (
+      <div className="section-share section-outstanding-doctor">
+        <div className="section-container">
+          <div className="section-header">
+            <span className="title-section">Bác sĩ nổi bật tuần qua</span>
+            <button className="btn-section">Xem them</button>
+          </div>
+          <div className="section-body">
+            <Slider {...this.props.settings}>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Cơ xương khớp</div>
+                  </div>
+                </div>
+              </div>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Thần kinh</div>
+                  </div>
+                </div>
+              </div>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Tiêu hóa</div>
+                  </div>
+                </div>
+              </div>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Tim mạch</div>
+                  </div>
+                </div>
+              </div>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Tai mũi họng</div>
+                  </div>
+                </div>
+              </div>
+              <div className="section-customize">
+                <div className="customize-border">
+                  <div className="outer-bg">
+                    <div className="bg-image section-outstanding-doctor" />
+                  </div>
+                  <div className="position text-center">
+                    <div>Hippocrates</div>
+                    <div>Nha khoa</div>
+                  </div>
+                </div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
